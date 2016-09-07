9decd6165f63ffe274ce6dd4ef20164b78eeac24
Alex Creasy <acreasy@redhat.com>
2016-09-07 09:35:15 +0100

    Renamed app module to src

--- karma.conf.js
+++ karma.conf.js
@@ -41,8 +41,8 @@ module.exports = function(config) {
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
-      "app/root/**/module.js",
-      "app/root/**/*.js",
+      "src/root/**/module.js",
+      "src/root/**/*.js",
       "test/**/*.js"
     ],
 

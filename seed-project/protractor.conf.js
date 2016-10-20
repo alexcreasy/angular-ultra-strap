4b57dcdec9c9a96ce0cc976682c5a00e813f6a15
Alex Creasy <acreasy@redhat.com>
2016-10-20 12:11:18 +0100

    switched to firefox

--- seed-project/protractor.conf.js
+++ seed-project/protractor.conf.js
@@ -4,7 +4,7 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['test/e2e/**/*.js'],
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
   baseUrl: 'http://localhost:9001'
 };
